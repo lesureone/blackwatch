9ce6ffe051c04d4d3bf8976c9b79b19fe3dd9df0
Larry LeSure <lesure1@gmail.com>
2015-02-22 13:35:02 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blackwatch::Application.config.secret_key_base = '25919671ab050fb0309d997184ef5bcfe4d9bf5aaed5b9e5c90d484f53dd14813f7dc4707aa5c45ad8573dc3a69f3a0ef856e34c4efd4118cde052f7efffab12'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blackwatch::Application.config.session_store :cookie_store, key: '_blackwatch_session'
